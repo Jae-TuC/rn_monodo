13462abb1b7cd38fd6157a927913d63cdb15844a
jaecycis <justice@buildpack.app>
2025-01-30 15:53:54 +0100

    addedd lucide icon * svg

--- utils/cssintorop.ts
+++ utils/cssintorop.ts
@@ -1,6 +1,15 @@
 import { Stagger } from "@animatereactnative/stagger";
 import { LinearGradient } from "expo-linear-gradient";
+import { icons } from "lucide-react-native";
 import { cssInterop } from "nativewind";
+
+Object.keys(icons).forEach((key) => {
+  // @ts-ignore
+  const IconComponent = icons[key];
+  cssInterop(IconComponent, {
+    className: "style",
+  });
+});
 
 cssInterop(LinearGradient, {
   className: "style",
